04436a657af943329c5070b179e1c6ef0dc22be3
kjminn <kjminn2001@gmail.com>
2020-11-05 16:55:52 +0900

    Continue ViewFlipper

---
+++ app/src/main/java/kr/ac/ssu/multiprogressetc/DrawerActivity.java
@@ -0,0 +1,16 @@
+package kr.ac.ssu.multiprogressetc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawer);
+
+
+    }
+}
